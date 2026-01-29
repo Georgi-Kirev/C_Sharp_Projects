e6cdec22e8a128080ba073cb73867a12a033f4e2
Georgi-Kirev <gimly_666@abv.bg>
2026-01-29 11:10:38 +0200

    11_Exercise_Methods

---
+++ Programing_Fundamentals/11_Methods/06. Math Power/Program.cs
@@ -0,0 +1,16 @@
+﻿// Input
+
+int baseNum = int.Parse(Console.ReadLine());
+int powerNum = int.Parse(Console.ReadLine());
+
+
+// Display
+
+RaiseNumber(baseNum, powerNum);
+
+// Methods
+
+static void RaiseNumber(int baseNum, int powerNum)
+{
+    Console.WriteLine(Math.Pow(baseNum, powerNum));
+}
