83f5442a67bcad32a8168a95f4b6aa8dfe1f8d08
Georgi-Kirev <gimly_666@abv.bg>
2026-02-04 10:17:56 +0200

    13_Arrays_more

--- Programing_Fundamentals/13_Arrays/04. Magic Sum/Program.cs
+++ Programing_Fundamentals/13_Arrays/04. Magic Sum/Program.cs
@@ -4,3 +4,19 @@ int[] inputedNumbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray()
 int controlNumber = int.Parse(Console.ReadLine());
 
 // Checks
+
+for (int i = 0; i < inputedNumbers.Length - 1; i++)
+{
+    int currentElement = inputedNumbers[i];
+
+    for (int j = i + 1; j < inputedNumbers.Length; j++)
+    {
+        int nextRightElement = inputedNumbers[j];
+
+        if (currentElement + nextRightElement == controlNumber)
+        {
+            Console.WriteLine($"{currentElement} {nextRightElement}");
+            break;
+        }
+    }
+}
---
+++ Programing_Fundamentals/13_Arrays/7. Top Integers/Program.cs
@@ -0,0 +1,27 @@
+﻿// Input
+
+int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+
+for (int i = 0; i < input.Length; i++)
+{
+    int currentNumber = input[i];
+
+    bool topInteger = true;
+
+    for (int j = i + 1; j < input.Length; j++)
+    {
+        int nextNumber = input[j];
+
+        if (nextNumber >= currentNumber)
+        {
+            topInteger = false;
+            break;
+        }
+    }
+
+    if (topInteger)
+    {
+        Console.Write(currentNumber + " ");
+    }
+}
