61f59caa6274149cd3954d6adb6377437a7e5f8b
Georgi-Kirev <gimly_666@abv.bg>
2026-02-06 11:08:42 +0200

    14_Lists_Exercise

---
+++ Programing_Fundamentals/14_Lists/01. Change List/Program.cs
@@ -0,0 +1,27 @@
+﻿// Input
+
+List<int> inputString = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+string action = Console.ReadLine();
+
+// Calculation and Output
+
+while (action != "end")
+{
+
+
+    if (action.StartsWith("Delete"))
+    {
+        int num = int.Parse(action.Split(" ")[1]);
+        inputString.RemoveAll(element => element == num);
+    }
+    else if (action.StartsWith("Insert"))
+    {
+        int numToInsert = int.Parse(action.Split(" ")[1]);
+        int positionToInsert = int.Parse(action.Split(" ")[2]);
+        inputString.Insert(positionToInsert, numToInsert);
+    }
+
+    action = Console.ReadLine();
+}
+
+Console.WriteLine(string.Join(" ", inputString));
